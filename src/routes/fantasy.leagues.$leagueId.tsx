66a6abaab95c724c916c6bd6e1ef4a62c02100f8
mrdata007 <151575270+mrdata007@users.noreply.github.com>
2026-08-04 23:12:37 +0100

    style: format league detail access gate

--- src/routes/fantasy.leagues.$leagueId.tsx
+++ src/routes/fantasy.leagues.$leagueId.tsx
@@ -53,11 +53,7 @@ function LeagueDetailPage() {
   });
 
   if (source === "guest") {
-    return authStatus === "loading" ? (
-      <LoadingState />
-    ) : (
-      <FantasyAccessGate next="/fantasy/leagues" />
-    );
+    return authStatus === "loading" ? <LoadingState /> : <FantasyAccessGate next="/fantasy/leagues" />;
   }
 
   const league = leagueQ.data;
