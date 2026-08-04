2aef928dad982c56d311748aff06d475027829e5
mrdata007 <151575270+mrdata007@users.noreply.github.com>
2026-08-04 23:12:34 +0100

    style: format leagues access gate

--- src/routes/fantasy.leagues.tsx
+++ src/routes/fantasy.leagues.tsx
@@ -53,11 +53,7 @@ function LeaguesPage() {
   });
 
   if (source === "guest") {
-    return authStatus === "loading" ? (
-      <LoadingState />
-    ) : (
-      <FantasyAccessGate next="/fantasy/leagues" />
-    );
+    return authStatus === "loading" ? <LoadingState /> : <FantasyAccessGate next="/fantasy/leagues" />;
   }
 
   const leagues = remoteQ.data ?? [];
